ff61757e0816490bfe07a9def95d7c649b9e8f9c
drozd.an <drozd.a@ifuture.by>
2022-07-29 11:07:14 +0300

    add sass

--- components/MLink.js
+++ components/MLink.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import styles from '../styles/MLink.module.css'
 
-export default function ({text, href}) {
+export default function MLink ({text, href}) {
     return (
         <Link href={href}>
             <a className={styles.link}>{text}</a>
